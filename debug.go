b05168c1cdb83f4f919cc3f2226852ffa94ed95a
Jxck <block.rxckin.beats@gmail.com>
2013-09-27 20:27:26 +0900

    add debug.go

---
+++ debug.go
@@ -0,0 +1,18 @@
+package http2
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func Debug(str string) {
+	env := os.Getenv("DEBUG")
+	if strings.Contains(env, "http2") {
+		_, file, line, _ := runtime.Caller(1)
+		f := strings.Split(file, "/")
+		filename := f[len(f)-1]
+		fmt.Printf("%v:%v %v\n", filename, line, str)
+	}
+}
